1d249f51a715c34f243a88493d8baa24874c65ed
TootEgozy <tootegozy@gmail.com>
2023-03-28 21:08:01 +0300

    wip task manager tests

--- src/tests/unit/services/task-manager.test.ts
+++ src/tests/unit/services/task-manager.test.ts
@@ -6,32 +6,43 @@ import { User } from "../../../schemas/User";
 import { Task } from "../../../schemas/Task";
 import { SubTask } from "../../../schemas/SubTask";
 import sinon from 'sinon';
-import { TaskManagerError } from "../../../utils/ExpandedError";
+import { afterEach } from "mocha";
 
 chai.use(chaiAsPromised);
 
 describe('TaskManager', () => {
-    const models = {
-        User: getModelForClass(User),
-        Task: getModelForClass(Task),
-        SubTask: getModelForClass(SubTask),
-    };
-    const taskManager = new TaskManager(models);
-    describe('Create task', async () => {
-        const sandbox = sinon.createSandbox();
-        let saveStub: sinon.SinonStub;
-        let findByIdStub: sinon.SinonStub;
+  const models = {
+    User: getModelForClass(User),
+    Task: getModelForClass(Task),
+    SubTask: getModelForClass(SubTask),
+  };
+  const taskManager = new TaskManager(models);
 
+  describe('Create task', async () => {
+    const sandbox = sinon.createSandbox();
+    let taskSaveStub: sinon.SinonStub;
+    let userSaveStub: sinon.SinonStub;
+    let taskFindByIdStub: sinon.SinonStub;
+    let userFindByIdStub: sinon.SinonStub;
 
-        it('should create a task', async () => {
-            const err = new TaskManagerError('task manager has stopped responding');
-            expect(1).to.eq(1);
-        });
-        it('should fail to create a task if user is undefined', async () => {
-
-        });
-        it('should fail to create a task if saving it to user fails', async () => {
-
-        })
+    beforeEach(async() => {
+        taskSaveStub = sinon.stub(models.Task.prototype, 'save');
+        userSaveStub = sinon.stub(models.User.prototype, 'save');
+        taskFindByIdStub = sinon.stub(models.Task.prototype, 'findById');
+        userFindByIdStub = sinon.stub(models.User.prototype, 'findById');
     });
+    afterEach(() => sandbox.restore());
+
+    it('should create a task', async () => {
+      const taskData = { a: 'b' };
+
+    });
+
+    it('should fail to create a task if user is undefined', async () => {
+
+    });
+    it('should fail to create a task if saving it to user fails', async () => {
+
+    })
+  });
 });
